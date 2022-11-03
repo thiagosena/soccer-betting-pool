f2a042d96e37d831622c5f65e76ae1ff06e228a3
Thiago Sena <thiagosena.dev@gmail.com>
2022-11-03 14:57:07 -0300

    config: create server with prisma

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import cors from '@fastify/cors';
+import Fastify from 'fastify'
+import {PrismaClient} from '../prisma/prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+    return {count}
+  })
+
+  await fastify.listen({port: 3333})
+}
+
+bootstrap().then(r => {
+}).catch(error => {
+  console.log('error', error)
+})
