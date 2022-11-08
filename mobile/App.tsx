1a4ff4806610b05aa0a73d596ca25c49b56d8a0c
Thiago Sena <thiagosena.dev@gmail.com>
2022-11-08 11:38:09 -0300

    feat(mobile): create signIn and pool list screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,29 +1,31 @@
-import {NativeBaseProvider, StatusBar} from 'native-base';
-import {THEME} from './src/styles/theme';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { THEME } from './src/styles/theme';
 import {
   Roboto_400Regular,
   Roboto_500Medium,
   Roboto_700Bold,
   useFonts,
-} from '@expo-google-fonts/roboto'
-import {Loading} from './src/components/Loading';
-import {SignIn} from './src/screens/SignIn';
-
+} from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { Pools } from './src/screens/Pools';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
     Roboto_700Bold,
-  })
+  });
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn/> : <Loading/>}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Pools /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,67 @@
+import { createContext, ReactNode, useState, useEffect } from 'react';
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSessions from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  signIn: () => Promise<void>;
+  isUserLoading: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '739795511667-l0veekuom9ui7b4auajne4bdhgtqcnb5.apps.googleusercontent.com',
+    redirectUri: AuthSessions.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email'],
+  });
+
+  async function signIn() {
+    try {
+      setIsUserLoading(true)
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log('TOKEN DE AUTENTICAÇÃO ===>', access_token);
+  }
+
+  useEffect(() => {
+    if(response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  },[response])
+
+  return (
+    <AuthContext.Provider value={{
+      signIn,
+      isUserLoading,
+      user,
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
