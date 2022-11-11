9852519502a4bdb0d59f69579642f381ae764914
Thiago Sena <thiagosena.dev@gmail.com>
2022-11-11 13:13:50 -0300

    fix(mobile): adjust baseurl and button icon

--- mobile/src/components/ButtonIcon.tsx
+++ mobile/src/components/ButtonIcon.tsx
@@ -1,6 +1,7 @@
-import { TouchableOpacity, TouchableOpacityProps } from "react-native";
-import { IconProps } from "phosphor-react-native";
-import { useTheme } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { IconProps } from 'phosphor-react-native';
+import { useTheme } from 'native-base';
+import React from 'react';
 
 interface Props extends TouchableOpacityProps {
   icon: React.FC<IconProps>;
@@ -11,7 +12,7 @@ export function ButtonIcon({ icon: Icon, ...rest }: Props) {
 
   return (
     <TouchableOpacity {...rest}>
-      <Icon color={colors.gray[300]} size={sizes[6]} />
+      <Icon color={colors.gray[300]} size={sizes[6]}/>
     </TouchableOpacity>
   );
 }
